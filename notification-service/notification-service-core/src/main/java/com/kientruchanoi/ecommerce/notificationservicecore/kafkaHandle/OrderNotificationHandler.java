94e55b4f31208a49e831bc5d4b527d77729a4edc
cuonvc <cuong.fithau@gmail.com>
2024-05-29 22:21:43 +0700

    fix/Redis cache notification

--- notification-service/notification-service-core/src/main/java/com/kientruchanoi/ecommerce/notificationservicecore/kafkaHandle/OrderNotificationHandler.java
+++ notification-service/notification-service-core/src/main/java/com/kientruchanoi/ecommerce/notificationservicecore/kafkaHandle/OrderNotificationHandler.java
@@ -31,7 +31,13 @@ public class OrderNotificationHandler {
 //            redisTemplate.opsForValue().set(recipient, message.getPayload());
 
             NotificationBuilder value = redisTemplate.opsForValue().get(recipient); //tránh bị lặp thông báo khi server restart
-            if (value == null || !value.equals(message.getPayload())) {
+            //trường hợp đối với tạo tài khoản (có 1 noti nên dùng chung với topic của order luôn) thì
+            //nếu như tạo lần thứ hai trở đi với cùng 1 tài khoản -> FLUSHALL in Redis để tiếp tục
+            if (value == null ||
+                    !value.getDeviceToken().equals(message.getPayload().getDeviceToken()) ||
+                    !value.getRecipient().equals(message.getPayload().getRecipient()) ||
+                    !value.getFirebaseData().equals(message.getPayload().getFirebaseData())
+            ) {
                 log.info("CREATING...");
                 redisTemplate.opsForValue().set(recipient, message.getPayload());
                 notificationService.create(message.getPayload());
